038cf11b5783c9e68a6a7b11975b16c510c3ada8
Ricardo Schuller <ricardoschuller90@gmail.com>
2021-12-23 16:03:19 +0100

    In between frontend.

--- frontend/schuller-software-services/src/containers/Section.tsx
+++ frontend/schuller-software-services/src/containers/Section.tsx
@@ -1,15 +1,29 @@
 import React, {useState} from 'react';
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
 
-function Section(props:any) {
+interface SectionProps {
+    children?: React.ReactNode;
+    index?: number;
+    value?: number;
+}
 
-    const [state, setState] = useState(props);
+function Section(props:SectionProps) {
+
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event:React.SyntheticEvent, newValue:any) => {
+        setValue(newValue);
+    };
     return (
         <div className="Section">
-            <h1>Welcome to Schuller Software Services</h1>
-            <table>
-                <th></th>
-                <tr></tr>
-            </table>
+            <h1>Schuller Software Services</h1>
+            <Tabs value={value} onChange={handleChange} textColor="secondary" indicatorColor="secondary" >
+                <Tab value="0" label="Intro" />
+                <Tab value="1" label="Motivation"/>
+                <Tab value="2" label="Experience"/>
+            </Tabs>
+
         </div>
     );
 }
