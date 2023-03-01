80cabd69f11f8c7f8160271313fcce7767977553
Ricardo Schuller <ricardoschuller90@gmail.com>
2023-03-01 14:46:48 +0100

    Change message pk and sk.

--- api/api/src/main/java/com/schullersoftwareservices/model/Message.java
+++ api/api/src/main/java/com/schullersoftwareservices/model/Message.java
@@ -1,44 +1,52 @@
 package com.schullersoftwareservices.model;
 
-import lombok.Builder;
-import lombok.Data;
-import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.UUID;
+import lombok.Builder;
+import lombok.Data;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
 @Data
 @Builder
 public class Message {
 
-	public static final String DATE = "Date";
-	public static final String DATETIME = "DateTime";
-	static final String MESSAGE = "Message";
-	static final String OWNER = "Owner";
-	static final String UUID_STRING = "UUID";
+  static final String PK = "PK";
+  static final String SK = "SK";
+  static final String DATE = "Date";
+  static final String DATETIME = "DateTime";
+  static final String MESSAGE = "Message";
+  static final String OWNER = "Owner";
+  static final String UUID_STRING = "UUID";
 
-	UUID uuid;
-	String owner;
-	String message;
-	LocalDateTime dateTime;
-	LocalDate date;
+  UUID uuid;
+  String owner;
+  String message;
+  LocalDateTime dateTime;
+  LocalDate date;
 
-	public static Message fromMap(Map<String, AttributeValue> values) {
-		return Message.builder().date(LocalDate.parse(values.get(DATE).s()))
-				.dateTime(LocalDateTime.parse(values.get(DATETIME).s()))
-				.owner(values.get(OWNER).s())
-				.message(values.get(MESSAGE).s())
-				.uuid(UUID.fromString(values.get(UUID_STRING).s()))
-				.build();
-	}
+  public static Message fromMap(Map<String, AttributeValue> values) {
+    return Message.builder()
+        .date(LocalDate.parse(values.get(SK).s()))
+        .dateTime(LocalDateTime.parse(values.get(DATETIME).s()))
+        .owner(values.get(OWNER).s())
+        .message(values.get(MESSAGE).s())
+        .uuid(UUID.fromString(values.get(PK).s()))
+        .build();
+  }
 
-	public static Map<String, AttributeValue> toMap(Message message) {
-		return Map.of(UUID_STRING, AttributeValue.builder().s(message.getUuid().toString()).build(),
-				DATE, AttributeValue.builder().s(message.getDate().toString()).build(),
-				DATETIME, AttributeValue.builder().s(message.getDateTime().toString()).build(),
-				MESSAGE, AttributeValue.builder().s(message.getMessage()).build(),
-				OWNER, AttributeValue.builder().s(message.getOwner()).build());
-	}
+  public static Map<String, AttributeValue> toMap(Message message) {
+    return Map.of(
+        PK,
+        AttributeValue.builder().s(message.getUuid().toString()).build(),
+        SK,
+        AttributeValue.builder().s(message.getDate().toString()).build(),
+        DATETIME,
+        AttributeValue.builder().s(message.getDateTime().toString()).build(),
+        MESSAGE,
+        AttributeValue.builder().s(message.getMessage()).build(),
+        OWNER,
+        AttributeValue.builder().s(message.getOwner()).build());
+  }
 }
