3a81d3012d272a9c87fa8c3b2f22866140f196cc
Ricardo Schuller <ricardoschuller90@gmail.com>
2023-07-26 18:30:29 +0200

    Fix terraform.

--- infra/api-gateway.tf
+++ infra/api-gateway.tf
@@ -17,14 +17,3 @@ resource "aws_api_gateway_method" "api_method" {
   resource_id   = aws_api_gateway_resource.http_resource.id
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
 }
-
-resource "aws_api_gateway_integration" "http_integration" {
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_resource.http_resource.id
-  http_method = aws_api_gateway_method.api_method.http_method
-  integration_http_method = "POST"
-  type = "HTTP_PROXY"
-  uri = "/{proxy+}"
-  passthrough_behavior     = "WHEN_NO_MATCH"
-  request_parameters            = {"method.request.path.proxy" = true}
-}
